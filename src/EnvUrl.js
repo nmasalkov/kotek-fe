4802aa0816baba1bbe663193cfde3467d3e75ebe
Nikita Masalkov <nikita.masalkov@softswiss.com>
2023-07-09 22:09:26 +0200

    fix production

--- src/EnvUrl.js
+++ src/EnvUrl.js
@@ -1,8 +1,8 @@
 const EnvUrl = function() {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        return 'http://localhost:80/'
+        return 'http://localhost:3000/'
     } else {
-        return 'http://kotek-api.herokuapp.com/'
+        return 'http://localhost:3000/'
     }
 }
 
