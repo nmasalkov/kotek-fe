5fb530a1288c651bf30579f007d694e7e2fd445d
Nikita Masalkov <nikita.masalkov@softswiss.com>
2023-07-10 16:21:17 +0200

    fix env

--- src/EnvUrl.js
+++ src/EnvUrl.js
@@ -1,8 +1,8 @@
 const EnvUrl = function() {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        return 'http://localhost:3000/'
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://138.68.172.170:3000/'
     } else {
-        return 'http://api:3000/'
+        return 'http://138.68.172.170:3000/'
     }
 }
 
